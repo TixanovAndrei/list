6b50572423db6a25265e3b15536f4230b54f8945
Andruha <andtihanov@gmail.com>
2022-05-20 19:49:04 +0300

    Add push, pop tests

--- tests.c
+++ tests.c
@@ -13,6 +13,55 @@ int main()
 	else
 		fprintf(stdout, "Test_1.1\t->\tPASSED\n");
 
+    push(1, NULL, &err);
+    if (err != INVARG)
+		fprintf(stdout, "Test_2.1\t->\tFAILED\n");
+	else
+		fprintf(stdout, "Test_2.1\t->\tPASSED\n");
 
-return 0;
+    push(2, &list, &err);
+    if (err != SUCCESS)
+		fprintf(stdout, "Test_2.2\t->\tFAILED\n");
+	else
+		fprintf(stdout, "Test_2.2\t->\tPASSED\n");
+
+    int i = 0;
+    for (i = 3; i<= 10; i++) {
+        push(i, &list, &err);
+    }
+    if (err != SUCCESS)
+		fprintf(stdout, "Test_2.3\t->\tFAILED\n");
+	else
+		fprintf(stdout, "Test_2.3\t->\tPASSED\n");
+
+	pop(NULL, &err);
+	if (err != INVARG)
+		fprintf(stdout, "Test_3.1\t->\tFAILED\n");
+	else
+		fprintf(stdout, "Test_3.1\t->\tPASSED\n");
+
+	int n;
+	for (i = 0; i < 9; i++) {
+		n = pop(&list, &err);
+		printf("%d ", n);
+	}
+	printf("\n");
+	if (err != SUCCESS)
+		fprintf(stdout, "Test_3.2\t->\tFAILED\n");
+	else
+		fprintf(stdout, "Test_3.2\t->\tPASSED\n");
+
+	pop(&list, &err);
+	if (err != HEAD)
+		fprintf(stdout, "Test_3.3\t->\tFAILED\n");
+	else
+		fprintf(stdout, "Test_3.3\t->\tPASSED\n");
+
+	for (i = 2; i<= 10; i++) {
+        push(i, &list, &err);
+    }
+
+    
+
+    return 0;
 }
